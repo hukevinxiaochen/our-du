2404c9fbbbe5d0c9b12780238a08d7632934b6b6
hukevinxiaochen <hukevinxiaochen@gmail.com>
2016-11-28 14:55:37 -0500

    Write function for find. Consider using find instead of stat.
    Find actually gets the stats as it traverses the file
    hierarchy. This might be exactly what we need.
    
    Now to write an easy to use front-end for this!

--- our_du.py
+++ our_du.py
@@ -14,9 +14,10 @@ print("Welcome. We are using the python located here {}.".format(which_python))
 # Let's get down to business
 import subprocess
 
+# TODO: Consider using decorators to abstract these GNU utils
 # A function that takes file arguments and spits out the desired info.
-def stat(path_to_file):
-    """stat invokes gstat
+def stat(file_path):
+    """Invokes GNU stat (gstat on OS X)
 
     gstat is what we call the GNU coreutils `stat` package when we are on OS X.
 
@@ -30,11 +31,21 @@ def stat(path_to_file):
     TODO: Make this script work on Linux too.
     """
 
-    completed_process = subprocess.run(["gstat", "--format", "%s %x %w %n", path_to_file], stdout=subprocess.PIPE)
+    completed_process = subprocess.run(["gstat", "--format", "%s %x %w %n", file_path], stdout=subprocess.PIPE)
 
     # print(completed_process.stdout)
     stats = completed_process.stdout
     return stats
+
+def find(file_path):
+    """Invokes GNU find (gfind on OS X)
+    """
+
+    completed_process = subprocess.run(["gfind", file_path, "-printf", "%a %s %p"], stdout=subprocess.PIPE)
+
+    # print(completed_process.stdout)
+    found = completed_process.stdout
+    return found
 
 if __name__ == "__main__":
     stat("README.markdown")
