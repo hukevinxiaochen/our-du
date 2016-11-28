4862856e6b3d84e8b07de1f1a4f82d965589e1d0
hukevinxiaochen <hukevinxiaochen@gmail.com>
2016-11-28 06:45:02 -0500

    Define stats function, which returns formatted gstat output.
    Trying to use PEP8 docstrings.

--- my_du.py
+++ my_du.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-"""
+"""Use My DU to keep your files clean.
+
 SYSTEM REQUIREMENTS:
     - OS X 10.10.5
     - GNU coreutils installed via Homebrew
@@ -11,8 +12,27 @@ print("Welcome. We are using the python located here {}".format(which_python))
 
 import subprocess
 
-# Use data from stat
-# The process we are calling is gstat because this script assumes
-completed_process = subprocess.run(["ls", "-al"], stdout=subprocess.PIPE)
-print(completed_process.stdout)
+# Use data from GNU coreutils' stat.
+# We invoke it using the token `gstat` because OS X includes its own stat.
 
+def stat(path_to_file):
+    """stat invokes gstat
+
+    gstat is invoked with the --format option:
+
+    %s - size in bytes
+    %x - last accessed date
+    %w - birth date
+    %n - file name
+
+    TODO: Make this script work on Linux too.
+    """
+
+    completed_process = subprocess.run(["gstat", "--format", "%s %x %w %n", path_to_file], stdout=subprocess.PIPE)
+
+    # print(completed_process.stdout)
+    stats = completed_process.stdout
+    return stats
+
+if __name__ == "__main__":
+    stat("README.markdown")
