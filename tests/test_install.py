29dc6e463f261af7d7c4e98462f391828c5b0c30
hukevinxiaochen <hukevinxiaochen@gmail.com>
2016-11-29 14:47:50 -0500

    Clear out commented code in test.

--- tests/test_install.py
+++ tests/test_install.py
@@ -1,11 +1,3 @@
-# import subprocess
-
-# WHERE_SHIT_BE_AT = "/Users/KHU/Desktop/keep/tools/our_du"
-# SCRIPT_NAME = "our_du.py"
-
-# def test_it_is_executable():
-#    path_to_script = "{0}/{1}".format(WHERE_SHIT_BE_AT, SCRIPT_NAME)
-#    assert subprocess.call([path_to_script]) == 0
 import subprocess
 
 def test_command():
